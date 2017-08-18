00215dc4b6197d0077ab730917fead7ab5135d79
821453366 <821453366@qq.com>
2017-08-18 10:21:11 +0800

    完成section-2

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,66 +1,70 @@
 'use strict';
 
 function find(collection, ch) {
-    for (let item of collection) {
-        if (item.name === ch) {
-            return item;
-        }
-    }
 
-    return null;
+    return collection.find(item => item.name === ch);
 }
 
 function summarize(collection) {
     let result = [];
-    for (let item of collection) {
+
+    collection.forEach(item => {
         let obj = find(result, item)
         if (obj) {
             obj.summary++;
         } else {
             result.push({name: item, summary: 1});
         }
-    }
+    });
+
     return result;
 }
 
 function split(item) {
     if (item.includes("-")) {
         let array = item.split("-");
+
         return {key: array[0], count: parseInt(array[1], 10)};
     }
 
     if (item.includes("[")) {
         let key = item.charAt(0);
         let count = parseInt(item.substr(2, item.length - 1));
+
         return {key, count};
     }
 
     if (item.includes(":")) {
         let array = item.split(":");
+
         return {key: array[0], count: parseInt(array[1], 10)};
     }
 }
 
 function push(result, key, count) {
-    for (var i = 0; i < count; i++) {
+    let number = 1;
+
+    while (count >= number++)
         result.push(key);
-    }
 }
 
 function expand(collection) {
     let result = [];
-    for (let item of collection) {
+
+    collection.forEach(item => {
         if (item.length === 1) {
             result.push(item);
         } else {
             let {key, count} = split(item);
             push(result, key, count);
         }
-    }
+    });
+
     return result;
 }
 
 module.exports = function countSameElements(collection) {
     let expandedArray = expand(collection);
+
     return summarize(expandedArray);
 }
