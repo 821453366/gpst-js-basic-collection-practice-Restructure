9a963dc21644ee72830a058a9735bd0faea69412
821453366 <821453366@qq.com>
2017-08-18 10:31:52 +0800

    完成section-3

--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,77 +1,76 @@
 'use strict';
 
 function find(collection, ch) {
-    for (let item of collection) {
-        if (item.key === ch) {
-            return item;
-        }
-    }
 
-    return null;
+    return collection.find(item => item.key === ch);
 }
 
 function summarize(collection) {
     let result = [];
-    for (let item of collection) {
+
+    collection.forEach(item => {
         let obj = find(result, item)
         if (obj) {
             obj.count++;
         } else {
             result.push({key: item, count: 1});
         }
-    }
+    });
+
     return result;
 }
 
 function split(item) {
     let array = item.split("-");
+
     return {key: array[0], count: parseInt(array[1], 10)};
 }
 
 function push(result, key, count) {
-    for (var i = 0; i < count; i++) {
+    let number = 1;
+
+    while (count >= number++)
         result.push(key);
-    }
 }
 
 function expand(collection) {
     let result = [];
-    for (let item of collection) {
+
+    collection.forEach(item => {
         if (item.length === 1) {
             result.push(item);
         } else {
             let {key, count} = split(item);
             push(result, key, count);
         }
-    }
+    });
+
     return result;
 }
 
 function includes(collection, ch) {
-    for (let item of collection) {
-        if (item === ch) {
-            return true;
-        }
-    }
 
-    return false;
+    return collection.indexOf(ch) > -1;
 }
 
 function discount(collection, promotionItems) {
     let result = [];
-    for (let item of collection) {
+
+    collection.forEach(item => {
         let key = item.key;
         let count = item.count;
         if (includes(promotionItems, key)) {
             count = count - Math.floor(count / 3);
         }
         result.push({key, count});
-    }
+    });
+
     return result;
 }
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
     let expandedArray = expand(collectionA);
     let summarizedArray = summarize(expandedArray);
+
     return discount(summarizedArray, objectB.value);
 }
